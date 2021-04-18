679f6b932d7c7dc021d7c32204a361adb42659f4
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2021-04-18 11:30:12 +0700

    Update fenwick.cpp

--- fenwick.cpp
+++ fenwick.cpp
@@ -37,3 +37,39 @@ public:
 		return ret;
 	}
 };
+template<typename T>
+class suffbit{
+	uint32_t n;
+	std::vector<T> content;
+	inline static uint32_t up(uint32_t x){
+		return x|x+1;
+	}
+	inline static uint32_t down(uint32_t x){
+		return x-(x+1&~x);
+	}
+public:
+	suffbit():n(0){}
+	suffbit(uint32_t n):n(n), content(n){}
+	void build(){
+		for(uint32_t i = n; i--;){
+			if(down(i)!=-1)
+				content[down(i)]=content[i]+content[down(i)];
+		}
+	}
+	void update(uint32_t x, T y){
+		for(; x!=-1; x=down(x))
+			content[x]=y+content[x];
+	}
+	T query(uint32_t x){
+		T ret;
+		for(; x < n; x=up(x))
+			ret=ret+content[x];
+		return ret;
+	}
+	T operator[](uint32_t x){
+		T ret;
+		for(; x < n; x=up(x))
+			ret=ret+content[x];
+		return ret;
+	}
+};
