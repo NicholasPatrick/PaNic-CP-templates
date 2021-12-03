5714acd46a9b3371534d4c5be4b82eef7197dfa4
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2021-12-04 01:17:53 +0800

    Update centroid.cpp

--- centroid.cpp
+++ centroid.cpp
@@ -1,38 +1,38 @@
+#include <bitset>
 #include <cstdio>
 #include <queue>
-#include <bitset>
 
-const int maxn=1e5;
+const int maxn = 1e5;
 std::vector<int> adjLis[maxn];
 std::bitset<maxn> mark;
 int sz[maxn];
-int getsz(int x, int p=-1){
+int getsz(int x, int p = -1) {
 	sz[x]=1;
-	for(int y : adjLis[x]){
-		if(y==p or mark[y])
+	for (int y : adjLis[x]) {
+		if (y == p or mark[y])
 			continue;
-		sz[x]+=getsz(y);
+		sz[x] += getsz(y);
 	}
 	return sz[x];
 }
-int getcent(int x, int ts){
-	for(int y : adjLis[x]){
-		if(sz[y]>sz[x] or mark[y])
+int getcent(int x, int ts) {
+	for (int y : adjLis[x]){
+		if (sz[y] > sz[x] or mark[y])
 			continue;
-		if(sz[y]>ts>>1)
+		if (sz[y] > ts >> 1)
 			return getcent(y, ts);
 	}
 	return x;
 }
-void centdecomp(int x=0){
-	mark[x=getcent(x, getsz(x))]=true;
+void centdecomp(int x = 0) {
+	mark[x = getcent(x, getsz(x))] = true;
 	/*
 		do stuff here
 	*/
-	for(int y : adjLis[x]){
-		if(mark[y])
+	for (int y : adjLis[x]) {
+		if (mark[y])
 			continue;
-		//centroid parent of y is x
+		// centroid parent of y is x
 		centdecomp(y);
 	}
 }
