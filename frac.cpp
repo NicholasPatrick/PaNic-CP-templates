4967a925473b25d42e7f9bea96e614372dedf96d
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2021-12-04 01:17:41 +0800

    Update frac.cpp

--- frac.cpp
+++ frac.cpp
@@ -1,63 +1,86 @@
-template <typename T>
-class frac{
+template <typename T, typename T2 = T>
+class frac {
 	T num, den;
-	frac():num(0), den(1){}
-	frac(T num):num(num), den(1){}
-	frac(T num, T den):num(num), den(den){}
-	frac operator+(frac rhs)const{
-		if(rhs.den==den){
-			rhs.num=num+rhs.num;
+	frac() : num(0), den(1) {}
+	frac(T num) : num(num), den(1) {}
+	frac(T num, T den) : num(num), den(den) {}
+	frac operator+(frac rhs) const {
+		if(rhs.den == den){
+			rhs.num = num + rhs.num;
 			return rhs;
 		}
-		rhs.num=rhs.den*num+rhs.num*den;
-		rhs.den=den*rhs.den;
+		rhs.num = (T2)rhs.den * num + (T2)rhs.num * den;
+		rhs.den = (T2)den * rhs.den;
 		return rhs;
 	}
-	frac operator-(frac rhs)const{
-		if(rhs.den==den){
-			rhs.num=num-rhs.num;
+	frac operator-(frac rhs) const {
+		if(rhs.den == den){
+			rhs.num = num-rhs.num;
 			return rhs;
 		}
-		rhs.num=rhs.den*num-rhs.num*den;
-		rhs.den=den*rhs.den;
+		rhs.num = (T2)rhs.den * num - (T2)rhs.num * den;
+		rhs.den = (T2)den * rhs.den;
 		return rhs;
 	}
-	frac operator*(frac rhs)const{
-		if(rhs.num==den){
-			rhs.num=num;
+	frac operator*(frac rhs) const {
+		if(rhs.num == den){
+			rhs.num = num;
 			return rhs;
 		}
-		if(rhs.den==num){
-			rhs.den=den;
+		if(rhs.den == num){
+			rhs.den = den;
 			return rhs;
 		}
-		rhs.num*=num;
-		rhs.den*=den;
+		rhs.num *= num;
+		rhs.den *= den;
 		return rhs;
 	}
-	frac operator/(frac rhs)const{
+	frac operator/(frac rhs) const {
 		//don't divide by 0
 		swap(rhs.num, rhs.den);
-		if(rhs.num==den){
-			rhs.num=num;
+		if(rhs.num == den){
+			rhs.num = num;
 			return rhs;
 		}
-		if(rhs.den==num){
-			rhs.den=den;
+		if(rhs.den == num){
+			rhs.den = den;
 			return rhs;
 		}
-		rhs.num*=num;
-		rhs.den*=den;
+		rhs.num *= num;
+		rhs.den *= den;
 		return rhs;
 	}
-	frac& operator+=(frac rhs){return *this=operator+(rhs);}
-	frac& operator-=(frac rhs){return *this=operator-(rhs);}
-	frac& operator*=(frac rhs){return *this=operator*(rhs);}
-	frac& operator/=(frac rhs){return *this=operator/(rhs);}
-	bool operator<(frac rhs)const{return num*rhs.den<rhs.num*den;}
-	bool operator<=(frac rhs)const{return num*rhs.den<=rhs.num*den;}
-	bool operator>(frac rhs)const{return num*rhs.den>rhs.num*den;}
-	bool operator>=(frac rhs)const{return num*rhs.den>=rhs.num*den;}
-	bool operator==(frac rhs)const{return num*rhs.den==rhs.num*den;}
-	bool operator!=(frac rhs)const{return num*rhs.den!=rhs.num*den;}
+	frac& operator+=(frac rhs) {
+		return *this = operator+(rhs);
+	}
+	frac& operator-=(frac rhs) {
+		return *this = operator-(rhs);
+	}
+	frac& operator*=(frac rhs) {
+		return *this = operator*(rhs);
+	}
+	frac& operator/=(frac rhs) {
+		return *this = operator/(rhs);
+	}
+	// auto operator<=>(frac rhs) const {
+	// 	return (T2)num * rhs.den <=> (T2)rhs.num * den;
+	// }
+	bool operator<(frac rhs) const {
+		return (T2)num * rhs.den < (T2)rhs.num * den;
+	}
+	bool operator<=(frac rhs) const {
+		return (T2)num * rhs.den <= (T2)rhs.num * den;
+	}
+	bool operator>(frac rhs) const {
+		return (T2)num * rhs.den > (T2)rhs.num * den;
+	}
+	bool operator>=(frac rhs) const {
+		return (T2)num * rhs.den >= (T2)rhs.num * den;
+	}
+	bool operator==(frac rhs) const {
+		return (T2)num * rhs.den == (T2)rhs.num * den;
+	}
+	bool operator!=(frac rhs) const {
+		return (T2)num * rhs.den != (T2)rhs.num * den;
+	}
 };
