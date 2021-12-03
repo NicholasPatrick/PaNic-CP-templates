671109e6b87b517562ccb65f1b05bd34aac1446b
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2021-12-04 01:17:48 +0800

    Update exponent.cpp

--- exponent.cpp
+++ exponent.cpp
@@ -1,16 +1,14 @@
 #include <cstdint>
 
 template <typename T>
-T mulexp(T a, T b, uint32_t e){
-	//a*b^e
-	for(; e; e>>=1, b=b*b)
-		if(e&1)a=a*b;
+T mulexp(T a, T b, uint32_t e) {
+	for (; e; e >>= 1, b = b * b)
+		if (e & 1)a = a * b;
 	return a;
 }
 template <typename T>
-T expmul(T b, uint32_t e, T a){
-	//b^e*a
-	for(; e; e>>=1, b=b*b)
-		if(e&1)a=b*a;
+T expmul(T b, uint32_t e, T a) {
+	for (; e; e >>= 1, b = b * b)
+		if (e & 1)a = b * a;
 	return a;
 }
