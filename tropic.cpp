6a8a52280fb225958fa37596f33d07579c79d113
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2021-12-04 01:17:31 +0800

    Update tropic.cpp

--- tropic.cpp
+++ tropic.cpp
@@ -1,33 +1,73 @@
 //tropic.cpp
 template <typename T>
-struct tropicmin{
+struct tropicmin {
 	T a;
-	tropicmin(){}
-	tropicmin(T a):a(a){}
-	tropicmin operator+(tropicmin rhs)const{return min(a, rhs.a);}
-	tropicmin operator*(tropicmin rhs)const{return a+rhs.a;}
-	tropicmin operator+=(tropicmin rhs){return a=min(a, rhs.a);}
-	tropicmin operator*=(tropicmin rhs){return a=a+rhs.a;}
-	bool operator<(tropicmin rhs)const{return a<rhs.a;}
-	bool operator<=(tropicmin rhs)const{return a<=rhs.a;}
-	bool operator>(tropicmin rhs)const{return a>rhs.a;}
-	bool operator>=(tropicmin rhs)const{return a>=rhs.a;}
-	bool operator!=(tropicmin rhs)const{return a!=rhs.a;}
-	bool operator==(tropicmin rhs)const{return a==rhs.a;}
+	tropicmin() {}
+	tropicmin(T a) : a(a) {}
+	tropicmin operator+(tropicmin rhs) const {
+		return std::min(a, rhs.a);
+	}
+	tropicmin operator*(tropicmin rhs) const {
+		return a + rhs.a;
+	}
+	tropicmin operator+=(tropicmin rhs) {
+		return a = std::min(a, rhs.a);
+	}
+	tropicmin operator*=(tropicmin rhs) {
+		return a += rhs.a;
+	}
+	bool operator<(tropicmin rhs) const {
+		return a < rhs.a;
+	}
+	bool operator<=(tropicmin rhs) const {
+		return a <= rhs.a;
+	}
+	bool operator>(tropicmin rhs) const {
+		return a > rhs.a;
+	}
+	bool operator>=(tropicmin rhs) const {
+		return a >= rhs.a;
+	}
+	bool operator!=(tropicmin rhs) const {
+		return a != rhs.a;
+	}
+	bool operator==(tropicmin rhs) const {
+		return a == rhs.a;
+	}
 };
 template <typename T>
-struct tropicmax{
+struct tropicmax {
 	T a;
-	tropicmax(){}
-	tropicmax(T a):a(a){}
-	tropicmax operator+(tropicmax rhs)const{return min(a, rhs.a);}
-	tropicmax operator*(tropicmax rhs)const{return a+rhs.a;}
-	tropicmax operator+=(tropicmax rhs){return a=min(a, rhs.a);}
-	tropicmax operator*=(tropicmax rhs){return a=a+rhs.a;}
-	bool operator<(tropicmax rhs)const{return a<rhs.a;}
-	bool operator<=(tropicmax rhs)const{return a<=rhs.a;}
-	bool operator>(tropicmax rhs)const{return a>rhs.a;}
-	bool operator>=(tropicmax rhs)const{return a>=rhs.a;}
-	bool operator!=(tropicmax rhs)const{return a!=rhs.a;}
-	bool operator==(tropicmax rhs)const{return a==rhs.a;}
+	tropicmax() {}
+	tropicmax(T a) : a(a) {}
+	tropicmax operator+(tropicmax rhs) const {
+		return std::max(a, rhs.a);
+	}
+	tropicmax operator*(tropicmax rhs) const {
+		return a + rhs.a;
+	}
+	tropicmax operator+=(tropicmax rhs) {
+		return a = std::max(a, rhs.a);
+	}
+	tropicmax operator*=(tropicmax rhs) {
+		return a += rhs.a;
+	}
+	bool operator<(tropicmax rhs) const {
+		return a < rhs.a;
+	}
+	bool operator<=(tropicmax rhs) const {
+		return a <= rhs.a;
+	}
+	bool operator>(tropicmax rhs) const {
+		return a > rhs.a;
+	}
+	bool operator>=(tropicmax rhs) const {
+		return a >= rhs.a;
+	}
+	bool operator!=(tropicmax rhs) const {
+		return a != rhs.a;
+	}
+	bool operator==(tropicmax rhs) const {
+		return a == rhs.a;
+	}
 };
