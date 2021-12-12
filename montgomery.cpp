43eb19c987e97c9188996d1b3a2ab1b990074242
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2021-12-12 23:33:38 +0800

    Update montgomery.cpp

--- montgomery.cpp
+++ montgomery.cpp
@@ -21,6 +21,7 @@ using int128_t = __int128_t;
 using uint128_t = __uint128_t;
 template<uint64_t mod>
 class montgomery {
+	// guarantees v is always in [0, mod)
 	static_assert(mod & 1, "modulus must be odd");
 	static_assert(mod < 0x8000000000000000, "modulus must fit in 63 bits");
 	using T1 = std::conditional_t<(mod < 0x80000000), uint32_t, uint64_t>;
@@ -156,4 +157,154 @@ public:
 		return v >= rhs.v;
 	}
 };
-using mint = montgomery<998244353>;
+template<uint64_t mod>
+class lazy_montgomery {
+	// guarantees v is always in [0, mod * 2]
+	static_assert(mod & 1, "modulus must be odd");
+	static_assert(mod < 0x4000000000000000, "modulus must fit in 62 bits");
+	using T1 = std::conditional_t<(mod < 0x40000000), uint32_t, uint64_t>;
+	using T2 = std::conditional_t<(mod < 0x40000000), uint64_t, uint128_t>;
+	using S1 = std::make_signed<T1>;
+	using S2 = std::make_signed<T2>;
+	template<typename S1>
+	static constexpr S1 inv_normal(S1 n) {
+		S1 w = 1, x = 0, y = m;
+		while (n != 1) {
+			S1 z = y / n;
+			y -= n * z;
+			x -= w * z;
+			std::swap(y, n);
+			std::swap(w, x);
+		}
+		return w < 0 ? w + m : w;
+	}
+	static const T1 m = mod;
+	static const T1 m2 = m << 1;
+	static const T1 r = (T1)-1 % m + 1;
+	static const T1 r2 = (T2)-1 % m + 1;
+	static const T1 r3 = ((T2)r2 << 32) % m;
+	static const T1 nim = -inv_int(m);
+	static constexpr T1 reduce(T2 a) {
+		return (a + (T1)a * nim * (T2)m) >> sizeof(T1) * 8;
+	}
+	T1 v;
+public:
+	lazy_montgomery() : v(0) {}
+	lazy_montgomery(T1 init) : v(reduce((T2)r2 * init)) {}
+	T1 eval() {
+		T1 ret = reduce(v);
+		if (ret >= m2)
+			ret -= m2;
+		if (ret >= m)
+			ret -= m;
+		return ret;
+	}
+	lazy_montgomery& operator+=(lazy_montgomery rhs) {
+		v += rhs.v;
+		if (v >= m2)
+			v -= m2;
+		return *this;
+	}
+	lazy_montgomery& operator-=(lazy_montgomery rhs) {
+		v -= rhs.v;
+		if (v > m2)
+			v += m2;
+		return *this;
+	}
+	lazy_montgomery& operator*=(lazy_montgomery rhs) {
+		v = reduce((T2)v * rhs.v);
+		return *this;
+	}
+	lazy_montgomery& operator/=(lazy_montgomery rhs) {
+		v = reduce((T2)v * reduce((T2)r3 * inv_normal(rhs.v)));
+		return *this;
+	}
+	template<typename T>
+	lazy_montgomery& operator^=(T rhs) {
+		static_assert(std::is_integral_v<T>);
+		lazy_montgomery b = *this;
+		v = r;
+		for (; rhs; rhs >>= 1) {
+			if (rhs & 1)
+				operator*=(b);
+			b *= b;
+		}
+		return *this;
+	}
+	lazy_montgomery operator+(lazy_montgomery rhs) const {
+		auto ret = *this;
+		return ret += rhs;
+	}
+	lazy_montgomery operator-(lazy_montgomery rhs) const {
+		auto ret = *this;
+		return ret -= rhs;
+	}
+	lazy_montgomery operator*(lazy_montgomery rhs) const {
+		auto ret = *this;
+		return ret *= rhs;
+	}
+	lazy_montgomery operator/(lazy_montgomery rhs) const {
+		auto ret = *this;
+		return ret /= rhs;
+	}
+	template<typename T>
+	lazy_montgomery operator^(T rhs) const {
+		static_assert(std::is_integral_v<T>);
+		lazy_montgomery b = *this;
+		lazy_montgomery ret = 1;
+		for (; rhs; rhs >>= 1) {
+			if (rhs & 1)
+				ret *= b;
+			b *= b;
+		}
+		return ret;
+	}
+	lazy_montgomery operator-() const {
+		v = m2 - v;
+		return *this;
+	}
+	lazy_montgomery operator+() const {
+		return *this;
+	}
+	lazy_montgomery& operator++() {
+		v += r;
+		if (v >= m)
+			v -= m;
+		return *this;
+	}
+	lazy_montgomery& operator--() {
+		v -= r;
+		if (v > m2)
+			v += m;
+		return *this;
+	}
+	lazy_montgomery operator++(int) {
+		auto ret = *this;
+		operator++();
+		return ret;
+	}
+	lazy_montgomery operator--(int) {
+		auto ret = *this;
+		operator--();
+		return ret;
+	}
+	bool operator==(lazy_montgomery rhs) const {
+		return (v - rhs.v) % m == 0;
+	}
+	bool operator!=(lazy_montgomery rhs) const {
+		return (v - rhs.v) % m != 0;
+	}
+	bool operator<(lazy_montgomery rhs) const {
+		return v % m < rhs.v % m;
+	}
+	bool operator>(lazy_montgomery rhs) const {
+		return v % m > rhs.v % m;
+	}
+	bool operator<=(lazy_montgomery rhs) const {
+		return v % m <= rhs.v % m;
+	}
+	bool operator>=(lazy_montgomery rhs) const {
+		return v % m >= rhs.v % m;
+	}
+};
+using mint = lazy_montgomery<998244353>;
