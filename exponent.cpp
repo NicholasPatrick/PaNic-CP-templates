4ea6f93e0f75508533bede74d6f8e886c3541dda
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2022-02-27 03:00:15 +0800

    Update exponent.cpp

--- exponent.cpp
+++ exponent.cpp
@@ -6,3 +6,8 @@ template <class v_t, class e_t>
 v_t expmul(v_t b, e_t e, v_t a) {
 	for (; e; e >>= 1, b *= b) if (e & 1) a = b * a; return a;
 }
+u32 modexp(u32 b, auto e, u32 m) {
+	u32 r = 1;
+	for (; e; e >>= 1, b = (u64) b * b % m) if (e & 1) r = (u64) r * b % m;
+	return r;
+}
