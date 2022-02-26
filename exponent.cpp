fdb8e238dc113cdcdaf27d805a0cea3d5c077c01
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2022-02-27 02:51:43 +0800

    Update exponent.cpp

--- exponent.cpp
+++ exponent.cpp
@@ -1,8 +1,8 @@
-template <class T>
-T mulexp(T a, T b, u32 e) {
+template <class v_t, class e_t>
+v_t mulexp(v_t a, v_t b, e_t e) {
 	for (; e; e >>= 1, b *= b) if (e & 1) a *= b; return a;
 }
-template <class T>
-T expmul(T b, u32 e, T a) {
+template <class v_t, class e_t>
+v_t expmul(v_t b, e_t e, v_t a) {
 	for (; e; e >>= 1, b *= b) if (e & 1) a = b * a; return a;
 }
