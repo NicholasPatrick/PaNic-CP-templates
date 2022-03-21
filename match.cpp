4e8fc14819eaed3aa631b9f7e5556cd3f032d740
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2022-03-22 00:21:14 +0800

    Update match.cpp

--- match.cpp
+++ match.cpp
@@ -1,17 +1,22 @@
 auto match(u32 l, u32 r, const vector<pair<u32, u32>>& e) {
-	g_dir<true> g(l); vector<u32> rl(r, -1); vector<bool> lr(l, 0), vi; bool f;
-	for (auto [u, v] : e)
-		if (!~(rl[v] + lr[u])) lr[rl[v] = u] = 1; else g.add_edge(u, v);
+	vector<u32> rl(r, -1), d(l); vector<bool> lr(l, 0), vi; bool f;
+	for (auto [u, v] : e) {
+		++d[u];
+		if (!~(rl[v] + lr[u])) lr[rl[v] = u] = 1;
+	}
+	vector<vector<u32>> g(l); for (u32 i = l; i--;) g[i].reserve(d[i]);
+	for (auto [u, v] : e) g[u].push_back(v);
 	auto dfs = [&](auto f, u32 u) -> bool {
 		vi[u] = 1;
 		for (auto it = g[u].begin(); it != g[u].end(); ++it) {
 			u32 v = *it;
-			if (!~rl[v]) {rl[v] = u; g.erase(it); return 1;}
+			if (!~rl[v]) {rl[v] = u; return 1;}
 		}
 		for (auto it = g[u].begin(); it != g[u].end(); ++it) {
 			u32 v = *it;
-			if (!vi[rl[v]] && f(f, rl[v])) {g.add_edge(rl[v], v);
-				rl[v] = u; g.erase(it); return 1;}
+			if (!vi[rl[v]] && f(f, rl[v])) {
+				g.add_edge(rl[v], v); rl[v] = u; return 1;
+			}
 		}
 		return 0;
 	};
